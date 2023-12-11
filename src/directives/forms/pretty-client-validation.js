ded5c6bff298ad74dea4dac5f2d72f23b9cf21fd
Nicolas Rio <nicolas.rio@cgi.com>
2023-12-11 13:32:26 +0100

    WIP

--- src/directives/forms/pretty-client-validation.js
+++ src/directives/forms/pretty-client-validation.js
@@ -8,24 +8,42 @@ export const prettyClientValidationDirective = {
             throw new Error("prettyClientValidation should be used on a form element.")
         }
 
+        element.setAttribute("novalidate", "")
+
+        /**
+         * @param {Event} event 
+         */
+        const inputEventListener = (event) => {
+            // @ts-expect-error
+            handleInputError(event.target);
+        }
+
         /**
          * @param {SubmitEvent} event 
          */
-        const eventListener = (event) => {
+        const submitEventListener = (event) => {
             event.preventDefault();
+
+            if (!element.dataset.submited) {
+                element.addEventListener("input", inputEventListener)
+            }
 
             element.dataset.submited = ""
 
             Array.from(element.querySelectorAll("input, select, textarea")).forEach(input => {
                 if (!isFormInputElement(input)) return;
                 handleInputError(input);
+                if (!input.validity.valid) input.dataset.hadErrors = ""
             })
         }
 
-        element.addEventListener("submit", eventListener)
+        element.addEventListener("submit", submitEventListener)
 
         return {
-            cleanup: () => element.removeEventListener("submit", eventListener)
+            cleanup: () => {
+                element.removeEventListener("submit", submitEventListener);
+                if (element.dataset.submited) element.removeEventListener("input", inputEventListener);
+            }
         }
     }
 }
@@ -39,6 +57,8 @@ function handleInputError(input) {
     }
 
     if (input.validity.valid) return;
+
+    console.log(input.validity)
 
     const errorLabel = document.createElement("label");
     errorLabel.setAttribute("for", input.id);
